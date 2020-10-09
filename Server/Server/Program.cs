e0d622c8f10359c8ebc04754f79d4afe25af8fcf
Сергей Коротков <rushero@LIVE.RU>
2020-10-09 05:22:57 +0300

    Переработка с нуля, создан механизм формрования частотного словника, процесс составления распараллелен на  70%.

---
+++ Server/Server/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "C:\\Users\\Ennea\\Desktop\\TMP.txt";
+            Console.WriteLine(file);
+            string tmpText = "";
+            try
+            {
+                if (File.Exists(file))
+                {
+                    using (StreamReader sr = new StreamReader(file, Encoding.Default))
+                    {
+                        tmpText = sr.ReadToEnd();
+                        sr.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Text text = new Text("NVM RIGHT NOW!", tmpText);
+            Console.WriteLine($"name = {text.name}\n");
+            Console.WriteLine($"Text = \n {text.text}\n");
+            List<Text> textl = text.ParseToTextes();
+            List<WordList> listWordList = new List<WordList>();
+            for (int l = 0; l < Environment.ProcessorCount; l++)
+            {
+                listWordList.Add(new WordList());
+            }
+            var startTime = System.Diagnostics.Stopwatch.StartNew();
+            var startTime2 = System.Diagnostics.Stopwatch.StartNew();
+            Task[] taskArray = new Task[Environment.ProcessorCount];
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                parseParams pp = new parseParams(listWordList.ElementAt(i), textl.ElementAt(i));
+                taskArray[i] = new Task(() => Parse(pp));
+                taskArray[i].Start();
+            }
+            Task.WaitAll(taskArray);
+            //text.Parse(wl);
+            startTime2.Stop();
+            var resultTime = startTime2.Elapsed;
+            Console.WriteLine($"Result time to complete code 2: {resultTime}\n");
+            WordList main = JoinWordLists(listWordList);
+            //main.Sort();
+            //Console.WriteLine(main.Print());
+            startTime.Stop();
+            resultTime = startTime.Elapsed;
+            Console.WriteLine($"Result time to complete code: {resultTime}\n");
+        }
+
+        public static WordList JoinWordLists(List<WordList> lwl)
+        {
+            WordList main = lwl[0];
+            for(int s = 1; s < lwl.Count - 1; s++)
+            {
+                for(int j = 0; j < lwl[s].count; j++)
+                {
+                    int n;
+                  if ((n = main.IndexOf(lwl[s].words[j].word)) > 0)
+                    {
+                        main.words[n].count += lwl[s].words[j].count;
+                    }
+                  else
+                    {
+                        main.Add(lwl[s].words[j].word, lwl[s].words[j].count);
+                    }
+                }
+            }
+            return main;
+        }
+
+        public static void Parse(object x)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} begin work\n");
+            parseParams pp = (parseParams)x;
+            Text txt = pp.txt;
+            WordList wl = pp.wl;
+            txt.Parse(wl);
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} complete work\n");
+        }
+
+        public class parseParams
+        {
+            public WordList wl;
+            public Text txt;
+
+            public parseParams(WordList wl, Text txt)
+            {
+                this.wl = wl;
+                this.txt = txt;
+            }
+        }
+    }
+}
---
+++ Server/Server/Text.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Drawing;
+
+namespace Server
+{
+    class Text
+    {
+        public string name;
+        public string text;
+
+        public Text(string name, string text)
+        {
+           this.name = name;
+           this.text = text;
+        }
+
+        public List<Text> ParseToTextes()
+        {
+            List<Text> textL = new List<Text>();
+            int coreCount = Environment.ProcessorCount;
+            int divider = Convert.ToInt32(Math.Round((double) (text.Length / coreCount)));
+            int pointbegin = 0;
+            int pointend = divider;
+            for (int i = 0; i < coreCount - 1; i++)
+            {
+                while (!Char.IsWhiteSpace(text, pointend))
+                {
+                    pointend++;
+                }
+                textL.Add(new Text("part", text.Substring(pointbegin, pointend - pointbegin)));
+                pointbegin = pointend;
+                pointend = pointbegin + divider;
+            }
+            textL.Add(new Text("part", text.Substring(pointbegin)));
+            return textL;
+        }
+
+        public void Parse(WordList wl)
+        {
+            Match m;
+            string HRefPatter = @"([[^\wА-Яа-я\'\-]+)";
+            m = Regex.Match(text, HRefPatter, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            while(m.Success)
+            {
+                int index = wl.IndexOf(m.Groups[1].Value);
+                if (index < 0)
+                {
+                    wl.Add(m.Groups[1].Value, 1);
+                    wl.count++;
+                    //Console.WriteLine("Добавленно новое слово!\n");
+                }
+                if (index >= 0)
+                {
+                    wl.Increase(m.Groups[1].Value);
+                    //Console.WriteLine("Увеличен счетчик уже имеющегося слова!\n");
+                }
+                m = m.NextMatch();
+            }
+        }
+    }
+
+    class WordList
+    {
+        //public ConcurrentQueue<Word> words;
+        public List<Word> words;
+        public int count;
+
+        public WordList()
+        {
+            //words = new ConcurrentQueue<Word>();
+            words = new List<Word>();
+            count = 0;
+        }
+        public int IndexOf(string cword)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if((words.ElementAt(i).word.CompareTo(cword)) == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Sort()
+        {
+            this.words.Sort((obj1, obj2) => obj1.count.CompareTo(obj2.count));
+        }
+
+        public void Add(string word, int count = 0)
+        {
+            //words.Enqueue(new Word(word, count));
+            words.Add(new Word(word, count));
+        }
+
+        public void Increase(string word)
+        {
+            words.ElementAt(this.IndexOf(word)).count += 1;
+        }
+
+        public string Print()
+        {
+            string tmp = "";
+            for(int i = 0; i < count; i++)
+            {
+                tmp += (words.ElementAt(i).Print() + "\n");
+            }
+            return tmp;
+        }
+    }
+    class Word : IComparable<Word>
+    {
+        public string word;
+        public int count;
+
+        public Word(string word, int count)
+        {
+            this.word = word;
+            this.count = count;
+        }
+
+        public int CompareTo(Word other)
+        {
+            if (Object.ReferenceEquals(other, this))
+                return 0;
+            else if (Object.ReferenceEquals(other, null))
+                return 1;
+
+            return this.count - other.count;
+        }
+        public string Print()
+        {
+            return (word + " -=- " + count);
+        }
+    }
+}
